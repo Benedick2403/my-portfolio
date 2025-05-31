142ee1aaa45a5df2d094f1df52547291b999d58a
Benedick2403 <bennn2403@gmail.com>
2025-05-31 13:58:08 +0800

    switch skills and role

--- src/components/Data/SkillsData.js
+++ src/components/Data/SkillsData.js
@@ -2,16 +2,17 @@ import SkillsUI from "../UI/SkillsUI";
 import { Gamepad, Code2 } from "lucide-react";
 
 const skillsData = [
+     {
+        title:"Web Development",
+        description: "Let’s turn your ideas into stunning websites. I design and build clean, responsive, and user-friendly interfaces that look great on any device. From layout to launch, I make sure your site not only stands out but also delivers a smooth and engaging experience.",
+        icon: Code2,
+    },
     {
         title: "Game Development",
         description: "Let’s bring your game ideas to life. From pixel-perfect assets to fully playable levels, I build engaging, responsive, and fun game experiences. Whether it’s crafting mechanics, designing levels, or polishing gameplay, I turn concepts into games that players love to explore.",
         icon: Gamepad,
     },
-    {
-        title:"Web Development",
-        description: "Let’s turn your ideas into stunning websites. I design and build clean, responsive, and user-friendly interfaces that look great on any device. From layout to launch, I make sure your site not only stands out but also delivers a smooth and engaging experience.",
-        icon: Code2,
-    },
+
     
 ];
 
--- src/components/UI/AboutMe.js
+++ src/components/UI/AboutMe.js
@@ -6,7 +6,7 @@ export default function AboutMe (){
         <div className="flex flex-col justify-evenly w-full h-full max-h-full mb-5 lg:w-1/2 lg:mb-0">
             <div>
                 <h1 className="md:text-5xl text-3xl font-semibold text-yellow-500">Benedick M. Mercado</h1>
-                <h3 className="md:text-lg sm:text-sm mt-1 text-gray-800">Game Developer | FrontEnd</h3>
+                <h3 className="md:text-lg sm:text-sm mt-1 text-gray-800">Front End | Game Developer</h3>
             </div>
             <div className="flex flex-col w-full h-full max-h-full gap-7 md:text-base sm:text-sm [&_b]:text-[#4195c2] text-gray-700 mt-3">
                 <p>I am a fourth-year Bachelor of Science in Computer Science student at Tarlac State University, with a strong passion for both web and game development. Over the past few years, I have dedicated myself to learning and building projects using various technologies and platforms. My experience includes developing interactive applications and games using <b>Unity</b> and <b>Godot</b>, where I primarily work with <b>C#</b> for scripting. I’ve also built responsive and visually appealing websites using <b>HTML5</b>, <b>CSS</b>, <b>React</b>, and <b>Tailwind</b>, which has allowed me to sharpen both my front-end development and design skills.</p>
